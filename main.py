0ccb254033bd8f9d8a5bdd06b4dcaf739d11ed23
ArielMoise23 <ariel.m23@gmail.com>
2024-07-08 16:26:34 -0300

    comments

---
+++ main.py
@@ -0,0 +1,9 @@
+#### connecting to spotidy api
+
+## getting playlist, and the lyrics
+
+## getting translation
+
+## creating pds file with the sobns and lyrics and translations
+
+## creatin table of contents
